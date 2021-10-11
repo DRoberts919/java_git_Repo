96b18a77cd679e56f59b79a097ca781069800d41
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-10-11 15:59:06 -0600

    second push of the day

--- src/prog/Main.java
+++ src/prog/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("hello world");
+        System.out.println("this is going to work or else i am going to be so mad that i cannot control myself.");
     }
 }
